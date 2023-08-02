1d536528c4b4e0e09e24e03f7a8e5f9f68f9e0ce
AV <nastulua@yandex.ru>
2023-08-02 20:06:47 +0300

    Добавлены решения заданий

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -3,6 +3,16 @@
 // является ли первое число квадратом второго
 
 
+Console.WriteLine("Введите первое целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int square = numberB * numberB;
+
+if (numberA == square)
+{Console.WriteLine($"{square} - квадрат числа {numberB}");}
+else{
+Console.WriteLine($"{numberA} не является квадратом числа {numberB}");}
 
 
 
--- Task04/Program.cs
+++ Task04/Program.cs
@@ -2,3 +2,19 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+
+Console.WriteLine("Введите первое целое число ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число");
+int z = Convert.ToInt32(Console.ReadLine());
+
+int max = x;
+
+if ( x > max ) max = x;
+if ( y > max ) max = y;
+if ( z > max ) max = z;
+
+Console.WriteLine(max);
