a315291f5a640fca28ae522e366b3efb42deab6f
AV <nastulua@yandex.ru>
2023-08-05 16:10:38 +0300

    Поправлено решение в соответствии с условием

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,16 +1,22 @@
 ﻿// Напишите программу, которая
 // принимает на вход трехзначное число и на выходе
 // показывает вторую цифру этого числа
-
-int SecondDigit(int num) 
+int SecondDigit(int num)
 {
     int secondDigit = num % 100 / 10;
     return (secondDigit);
 }
 
-int number = new Random().Next(100, 1000);
-Console.WriteLine($"Случайное число в диапазоне 100 - 999 -> {number}");
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
-int result = SecondDigit(number);
-Console.WriteLine($"Вторая цифра числа -> {result}");
+if (number > 99 && number < 1000)
+    {
+    int result = SecondDigit(number);
+    Console.WriteLine($"Вторая цифра числа -> {result}");
+    }
+else
+    {
+    Console.WriteLine("Вы ввели не трехзначное число");
+    }
 
