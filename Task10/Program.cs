7c2b0dae1099aaf0025c37e2c775fb7aab7eed1a
AV <nastulua@yandex.ru>
2023-08-05 15:05:25 +0300

    программа принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая
+// принимает на вход трехзначное число и на выходе
+// показывает вторую цифру этого числа
+
+int SecondDigit(int num) 
+{
+    int secondDigit = num % 100 / 10;
+    return (secondDigit);
+}
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число в диапазоне 100 - 999 -> {number}");
+
+int result = SecondDigit(number);
+Console.WriteLine($"Вторая цифра числа -> {result}");
+
