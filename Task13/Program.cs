0e201e5b2c2de33a913616456eccdc12efe4b5f7
AV <nastulua@yandex.ru>
2023-08-06 10:38:08 +0300

    программа выводит третью цифру заданного числа или сообщает, что третьей цифры нет

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая
+// выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+
+int ThreeDigit(int num)
+{
+    int secondDigit = num % 100;
+    int thirdDigit = secondDigit % 10;
+    return (thirdDigit);
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number > 999)
+{
+    number = number / 10;
+
+if (number > 99 && number < 1000)
+    {
+    int result = ThreeDigit(number);
+    Console.WriteLine($"Третья цифра числа -> {result}");
+    }
+}
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
