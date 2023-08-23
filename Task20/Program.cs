be59077c727047416ef2ba2974df6f90810bf3b5
AV <nastulua@yandex.ru>
2023-08-23 21:22:09 +0300

    Решение измененно по новым условиям

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Напишите программу, которая
 // принимает на вход координаты двух точек и
-// находит расстояние между ними в 2D
+// находит расстояние между ними в 3D
 // пространстве.
 // A (3,6); B (2,1) -> 5,09
 // A (7,-5); B (1,-1) -> 7,21
 
-double FindDistance(int xA, int yA, int xB, int yB)
+double FindDistance(int xA, int yA, int xB, int yB, int zA, int zB)
 {
-    return Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2));
+    return Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2) + Math.Pow(zA - zB, 2));
 }
 
 Console.WriteLine("Введите координаты первой точки");
@@ -15,12 +15,16 @@ Console.Write("X: ");
 int xCoordinateA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Y: ");
 int yCoordinateA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int zCoordinateA = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите координаты второй точки");
 Console.Write("X: ");
 int xCoordinateB = Convert.ToInt32(Console.ReadLine());
 Console.Write("Y: ");
 int yCoordinateB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int zCoordinateB = Convert.ToInt32(Console.ReadLine());
 
-double result = FindDistance(xCoordinateA, yCoordinateA, xCoordinateB, yCoordinateB);
+double result = FindDistance(xCoordinateA, yCoordinateA, zCoordinateA, xCoordinateB, yCoordinateB, zCoordinateB);
 Console.WriteLine($"Расстояние между точками -> {Math.Round(result, 2, MidpointRounding.ToZero)}");
