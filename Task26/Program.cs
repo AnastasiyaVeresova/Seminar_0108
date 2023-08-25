132aa057283acffa81714330299863724a1bb4d5
AV <nastulua@yandex.ru>
2023-08-25 21:33:58 +0300

    Добавлено решение

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int DigitCount(int num)
+{
+    int count = 0;
+    if(num == 0) return 1;
+    
+    while(num != 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Число {number} состоит из {DigitCount(number)} знаков.");
