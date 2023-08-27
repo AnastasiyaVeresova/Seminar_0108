a662ff59eeec720efa2abcad49c686dc7cc691b5
AV <nastulua@yandex.ru>
2023-08-28 00:09:56 +0300

    Цикл на вход принимает два числа и возводит первое в натуральную степень, соответствующую второму числу

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+int Exponentiation(int num, int exp)
+{
+    int res = 1;
+    for (int i = 1; i <= exp; i++)
+    {
+        
+        res *= num;
+    }
+    return res;
+}
+
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральную степень, в которую нужно это число возвести");
+int exponenta = Convert.ToInt32(Console.ReadLine());
+
+int exponentiation = Exponentiation(number, exponenta);
+
+if(exponenta < 0)
+{
+    Console.WriteLine("Введено некорректное число для возведения в степень");
+    return;
+}
+
+Console.WriteLine($"число {number} в степени {exponenta} = {exponentiation}");
+
+
