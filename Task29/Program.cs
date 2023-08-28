70e58c77d413eb8a439260b9c2eb5f0083dd17c5
AV <nastulua@yandex.ru>
2023-08-29 00:15:16 +0300

    Программа задает массив из 8 элементов и выводит их на экран

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа задает массив из 8 элементов и выводит их на экран
+
+
+void FillArr(int[] mass)
+{
+    int i = 0;
+    var random = new Random();
+    while(i < mass.Length)
+    {
+        mass[i] = random.Next(0, 99);
+        i++;
+    }
+}
+
+void PrintArr(int[] mass)
+{
+    int i = 0;
+    while(i < mass.Length - 1)
+    {
+        Console.Write($"{mass[i]}, ");
+        i++;
+    }
+    Console.Write($"{mass[i]}");
+}
+
+void PrintArr2(int[] mass)
+{
+    int i = 0;
+    while(i < mass.Length - 1)
+    {
+        Console.Write($"{mass[i]}, ");
+        i++;
+    }
+    Console.Write($"{mass[i]}");
+}
+
+
+int[] array = new int[8];
+FillArr(array);
+
+PrintArr(array);
+Console.Write(" -> [");
+PrintArr2(array);
+Console.Write("]");
+
