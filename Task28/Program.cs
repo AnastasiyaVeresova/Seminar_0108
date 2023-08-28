1419ea69d47238d387116af94a177f0a1268ea69
AV <nastulua@yandex.ru>
2023-08-29 00:29:53 +0300

    Added file

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int MultiplicationNums(int num)
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            mult *= i;
+        }
+    }
+    return mult;
+}
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine($"{number} -> не корректное чило");
+}
+
+else
+{
+    int multNums = MultiplicationNums(number);
+    Console.WriteLine($"{multNums} -> произведения чисел от 1 до {number}");
+}
