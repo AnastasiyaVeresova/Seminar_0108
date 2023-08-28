6c90bda71f00f5c1dc2af7c55359b103ab30cc99
AV <nastulua@yandex.ru>
2023-08-29 00:21:16 +0300

    программа на вход принимает число и выдает сумму цифр в числе

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программfна вход принимает число и выдает сумму цифр в числе
+
+
+int NumbersSum(int num)
+{
+
+    int sum = 0;
+    while (num > 0)
+    {
+        int digit = num % 10;
+        sum = sum + digit;
+        num /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр в числе {number} = {NumbersSum(number)}");
