19ab14a09a0b83d9f51eb713128db56e863f4254
AV <nastulua@yandex.ru>
2023-08-30 09:49:34 +0300

    массив, заполненный случайными положительными и трехзначными числами. //Программа показывает количество четных чисел в массиве.

---
+++ Task34/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задаем массив, заполненный случайными положительными и трехзначными числами.
+//Программа показывает количество четных чисел в массиве.
+
+int[] CreatePozitiveThreedigit(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i =0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("] ");
+}
+
+int PozitiveCountDigit(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0 ) count++;
+    }
+    return count;
+}
+
+int[] array = CreatePozitiveThreedigit(4, 100, 999);
+PrintArray(array);
+int result = PozitiveCountDigit(array);
+Console.Write($"Количество четных чисел в массиве -> {result}");
