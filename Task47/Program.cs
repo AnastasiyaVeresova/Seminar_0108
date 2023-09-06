6b6be3e72ea35787fec81a411a8cb7a45ae5e89d
AV <nastulua@yandex.ru>
2023-09-06 10:01:07 +0300

    Задаем двумерный массив m * n, заполненный случайными вещественными числами

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задаем двумерный массив m * n, заполненный случайными вещественными числами
+
+double[,] CreateMatrixRndDouble(int m, int n, int min, int max) 
+{
+    double[,] matrix = new double[m, n]; 
+    Random rnd = new Random();
+
+    for (int i = 0; i < m; i++) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            
+            matrix[i, j] = rnd.Next(min, max + 1) + rnd.NextDouble();
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrixDouble(double[,] matrix)
+{
+    int m = matrix.GetLength(0);
+    int n = matrix.GetLength(1);
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write($"{matrix[i, j], 7:F2}");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array = CreateMatrixRndDouble(3, 4, -100, 100);
+PrintMatrixDouble(array);
