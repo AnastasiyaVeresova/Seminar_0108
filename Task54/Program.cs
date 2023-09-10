a072a46bec29fe9eadc7f21b96e5bcdbac668fa2
AV <nastulua@yandex.ru>
2023-09-10 17:26:29 +0300

    Задаем двумерный массив. Программа упорядочит по убыванию элементы каждой строки двумерного массива

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задаем двумерный массив. Программа упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void RangeEachElementOfRows(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+
+    }
+}
+
+int[,] matrix = CreateMatrixRndInt(3, 3, 0, 10);
+PrintMatrix(matrix);
+Console.WriteLine();
+RangeEachElementOfRows(matrix);
+PrintMatrix(matrix);
+
