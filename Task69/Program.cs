e90ff4c0fb7520e4ab76a418d18d2a94b1ade9ae
AV <nastulua@yandex.ru>
2023-09-12 21:34:19 +0300

    Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии

--- Task69/Program.cs
+++ Task69/Program.cs
@@ -1,72 +1,33 @@
-﻿// Задача 69 Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+﻿/*Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8*/
 
-// int rows = ReadInt("Введите номер строки: ");
-// int colums = ReadInt("Введите номер столбца: ");
 
-int rows = 3;
-int colums = 4;
-int[,] numbers = new int[rows, colums];
-FillArray2D(numbers);
-PrintArray2D(numbers);
 
-double[] avgNumbers = new double[numbers.GetLength(1)];
-
-for (int i = 0; i < numbers.GetLength(1); i++)
+int Prompt(string message)
 {
-    double result = 0.0;
-    for (int j = 0; j < numbers.GetLength(0); j++)
-    {
-        result += numbers[j, i];
-    }
-    avgNumbers[i] = result / numbers.GetLength(0);
-}
-PrintArray(avgNumbers);
-
-
-// Заполнение массива рандомными вещественными числами
-void FillArray2D(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-        }
-    }
+ Console.Write(message);
+ int value = Convert.ToInt32(Console.ReadLine());
+ return value;
 }
 
-//  Функция вывода двумерного массива в терминал
-void PrintArray2D(int[,] array)
+int Validation(int num)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
+ while (num < 1)
+ {
+  num = Prompt("Неверно введено число. Введите натуральное число: ");
+ }
+ return num;
 }
 
-// Функция вывода массива в терминал 
-void PrintArray(double[] array)
+int Exponentiation(int num1, int num2) //3, 5; 3, 4; 3, 3; 3, 2; 3, 1; 3, 0;
 {
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
+ if (num2 == 0) return 1; // 1 
+ return num1 * Exponentiation(num1, num2 - 1); // Это стэк : 3, 5; 3, 4; 3, 3; 3, 2; 3, 1; 
+                                               // 3*3*3*3*3*1(из return) = 243
 }
-
-// Функция ввода 
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
+int numberOne = Prompt("Введите первое число: ");
+int numberTwo = Prompt("Введите второе число: ");
+numberTwo = Validation(numberTwo);
+Console.WriteLine(Exponentiation(numberOne, numberTwo));
