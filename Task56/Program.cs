d389ad38ff8b77037f3199bcc95fc21a0b50d5e8
AV <nastulua@yandex.ru>
2023-09-12 13:25:46 +0300

    Добавлены папки

--- Task56/Program.cs
+++ Task56/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задаем прямоугольный двумерный массив. Программа находит строку с наименьшей суммой элементов.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumElementsInEachRows(int[,] array)
+{
+    int[] sumNumbersRows = new int[array.GetLength(0)];
+    Console.WriteLine("Сумма элементов по строкам: ");
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sumNumbersRows[i] += array[i, j];
+
+        }
+        Console.WriteLine($"{i + 1} -> {sumNumbersRows[i]} ");
+    }
+
+    int minSum = sumNumbersRows[0];
+    int minRow = 1;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (sumNumbersRows[i] < minSum)
+        {
+            minSum = sumNumbersRows[i];
+            minRow = i + 1;
+        }
+    }
+    return minRow;
+}
+
+int[,] matrix = CreateMatrixRndInt(3, 4, 0, 10);
+
+PrintMatrix(matrix);
+int rowNumber = SumElementsInEachRows(matrix);
+Console.WriteLine($"Наименьшая сумма элементов в строке: {rowNumber}");
+
--- Task60/Program.cs
+++ Task60/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Формируем трёхмерный массив из неповторяющихся двузначных чисел. Программа построчно выводит массив, добавляя индексы каждого элемента.
+
+
+void CreateMatrixRndInt(int[,,] matrix)
+{
+    int count = 10;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] += count;
+                count += 3;
+            }
+
+        }
+    }
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    int depth = matrix.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < depth; k++)
+            {
+                Console.Write($" {matrix[i, j, k],5} ({i}, {j}, {k}) ");
+            }
+        }
+
+    }
+}
+
+int[,,] matrixDepth = new int[3, 3, 3];
+CreateMatrixRndInt(matrixDepth);
+PrintMatrix(matrixDepth);
