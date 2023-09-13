56f274b05b399f77ea62500afa00feccbf719d59
AV <nastulua@yandex.ru>
2023-09-13 09:35:41 +0300

    Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int Validation(int num)
+{
+    while(num < 1)
+    {
+        num = Prompt("Неверно введено число. Введите натуральное число: ");
+    }
+    return num;
+}
+
+int NaturalNumbers(int num)
+{
+    if(num == 1) return 1;
+    Console.Write($"{num} ");
+    return NaturalNumbers (num - 1);
+}
+
+int number = Prompt("Введите натуральное число: ");
+number = Validation(number);
+
+
+ Console.WriteLine(NaturalNumbers(number));
