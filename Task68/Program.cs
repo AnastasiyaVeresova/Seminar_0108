d3e8323ca76e96709d6ce3c2b5ee2c7dbed84dda
AV <nastulua@yandex.ru>
2023-09-13 12:19:15 +0300

    Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,63 +1,22 @@
-﻿int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+int FunctionAnkerman(int k, int m)
 {
-    int[,] matrix = new int[rows, columns];
-    Random rnd = new Random();
+    if (k == 0)
+    return m + 1;
 
-    for(int i = 0; i < rows; i++)
+    else
     {
-        for(int j = 0; j < columns; j++)
+        if (k !=0 && m == 0)
+        return FunctionAnkerman(k - 1, 1);
+
+        else
         {
-            matrix[i, j] = rnd.Next(min, max + 1);
+            return FunctionAnkerman(k - 1, FunctionAnkerman(k, m - 1));
         }
     }
-    return matrix;
 }
 
-void PrintMatrix(int[,] matrix)
-{
-    int rows = matrix.GetLength(0);
-    int columns = matrix.GetLength(1);
-
-    for(int i = 0; i < rows; i++)
-    {
-        for(int j = 0; j < columns; j++)
-        {
-            Console.Write($"{matrix[i, j], 5}");
-        }
-        Console.WriteLine();
-    }
-}
-
-void SumElementsInEachRows(int[,] array)
-{
-int minRow = 0;
-int minSumRow = 0;
-int sumRow = 0;
- int [] sumNumbersRows = new int[array.GetLength(0)];
-    Console.Write("Суммы элементов в каждой строке: ");
-
-for (int i = 0; i < array.GetLength(1); i++)
-{
-    minRow += sumNumbersRows[i];
-}
-for (int i = 0; i < array.GetLength(0); i++)
-{
-    
-    for (int j = 0; j < array.GetLength(1); j++) sumRow += sumNumbersRows[i];
-    
-     Console.Write($"{sumNumbersRows[i]} ");
-
-    if (sumRow < minRow)
-    {
-        minRow = sumRow;
-        minSumRow = i + 1;
-    }
-    sumRow = 0;
-}
-Console.WriteLine($"Наименьшая сумма элементов в строке: {minSumRow}");
-
-}
-
-int[,] matrix = CreateMatrixRndInt(3, 4, 0, 10);
-PrintMatrix(matrix);
-SumElementsInEachRows(matrix);
+int res = FunctionAnkerman(3, 11);
+Console.WriteLine(res);
